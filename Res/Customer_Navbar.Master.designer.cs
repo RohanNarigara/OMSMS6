f8a34e10dc8d0b40b363b3213f3118fda57cd7c9
vishvaslathiya <lathiyavishvas@gmail.com>
2024-04-10 22:56:01 +0530

    Add to Cart and Updated Navbar(Product Dynamic)

--- Res/Customer_Navbar.Master.designer.cs
+++ Res/Customer_Navbar.Master.designer.cs
@@ -33,6 +33,15 @@ namespace OMSMS6.Res
         protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
 
         /// <summary>
+        /// lblCartCount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblCartCount;
+
+        /// <summary>
         /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
